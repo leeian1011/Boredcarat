222e312f47c7f29bab9a32219ab7593f0bd57e64
lee <leeian1011@gmail.com>
2023-11-27 22:57:31 +0800

    chore: thread sleep timing an display spacing

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,23 @@ fn main() {
 
     let mut player: [cards::Card; 2] = [deck.draw(), cards::Card::placeholder()];
     println!("Player\n{} \r", player[0]);
-    let mut x = std::io::Stdout::lock(&std::io::stdout());
-    x.flush();
 
-    std::thread::sleep(std::time::Duration::from_millis(1500));
+    std::thread::sleep(std::time::Duration::from_secs(1));
 
     let mut banker: [cards::Card; 2] = [deck.draw(), cards::Card::placeholder()];
-    println!("Banker\n{} \r", banker[0]);
+    println!("Banker\n{} \r\n\n", banker[0]);
 
-    std::thread::sleep(std::time::Duration::from_millis(1500));
+    std::thread::sleep(std::time::Duration::from_secs(1));
 
     player[1] = deck.draw();
     println!("Player\n{} {} \r", player[0], player[1]);
 
-    std::thread::sleep(std::time::Duration::from_millis(1500));
+    std::thread::sleep(std::time::Duration::from_secs(1));
 
     banker[1] = deck.draw();
-    println!("Banker\n{} {} \r", banker[0], banker[1]);
+    println!("Banker\n{} {} \r\n\n", banker[0], banker[1]);
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
 
     let mut player_sum = Into::<i32>::into(player[0].value) + Into::<i32>::into(player[1].value);
     if player_sum >= 10 {
@@ -42,7 +42,7 @@ fn main() {
         }
     }
 
-    println!("Player: {}, Banker: {}", player_sum, banker_sum);
+    println!("Player: {}, Banker: {}\n\n", player_sum, banker_sum);
 
     if banker_sum == 9 {
         println!("Banker Wins");
@@ -64,7 +64,7 @@ fn main() {
     }
 
     if player_sum < 6 && banker_sum < 8 {
-        std::thread::sleep(std::time::Duration::from_millis(1500));
+        std::thread::sleep(std::time::Duration::from_secs(1));
         let new_card = deck.draw();
         println!("Player\n{} {} \n {} \r", player[0], player[1], new_card);
         player_sum = player_sum + Into::<i32>::into(new_card.value);
@@ -86,7 +86,7 @@ fn main() {
 
             exit(0);
         } else {
-            std::thread::sleep(std::time::Duration::from_millis(1500));
+            std::thread::sleep(std::time::Duration::from_secs(1));
             let new_card = deck.draw();
             println!("Banker\n{} {} \n {} \r", player[0], player[1], new_card);
             banker_sum = banker_sum + Into::<i32>::into(new_card.value);
@@ -111,7 +111,7 @@ fn main() {
     }
 
     if banker_sum < 6 {
-        std::thread::sleep(std::time::Duration::from_millis(1500));
+        std::thread::sleep(std::time::Duration::from_secs(1));
         let new_card = deck.draw();
         println!("Banker\n{} {} \n {} \r", banker[0], banker[1], new_card);
         banker_sum = banker_sum + Into::<i32>::into(new_card.value);
